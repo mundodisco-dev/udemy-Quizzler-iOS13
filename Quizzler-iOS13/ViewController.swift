a8da2ef54a139093b5a750202a934304a43b4d5a
Adrian Martinez <adrian@adrians-mac-mini.local>
2019-10-28 22:09:32 +0100

    challenge: initial setup

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        questionLabel.text = "Four + Two is equal to six"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
